c7a318ab06038feef5b10311b4ce18588c9f26d9
Harsh Ranjan <128585057+Harsh-git98@users.noreply.github.com>
2024-06-19 07:33:25 +0530

    Update Login.jsx

--- login_register/src/Login.jsx
+++ login_register/src/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/login', {email, password})
+        axios.post('https://login-register-backend-ed24.onrender.com/login', {email, password})
             .then(result => {
                 
                 if (result.data === "Success") {
